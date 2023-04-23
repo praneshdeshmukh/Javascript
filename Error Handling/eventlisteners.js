9986554a3a3aeb234eeff4e0f81fae3ab59023a3
Pranesh Deshmukh <praneshd2003@gmail.com>
2023-04-23 23:42:43 +0530

    Update eventlisteners.js

--- Error Handling/eventlisteners.js
+++ Error Handling/eventlisteners.js
@@ -1,3 +1,4 @@
+// JAVASCRIPT :
 // Event Listeners :-
 // 01. click
 let cbtn = document.getElementById("clk");
@@ -40,3 +41,66 @@ document.addEventListener("keyup",function(e){
     displaythis.style.color = "purple";
     displaythis.innerText = e.key + " : is pressed by you!"
 })
+// index.html
+// <!DOCTYPE html>
+// <html lang="en">
+//   <head>
+//     <meta charset="UTF-8" />
+//     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+//     <title>Event listeners - 27/03/2023</title>
+//     <link rel="stylesheet" href="style.css" />
+//   </head>
+//   <body>
+//     <div class="wrapper">
+//       <div class="btn">
+//         <button class="btn btn1" id="clk">Clickkk me</button>
+//         <button class="btn btn2" id="dbclickbtn">Double Click</button>
+//         <button class="btn btn3" id="mouseovr">Hover Me</button>
+//         <button class="btn btn4" id="mouseot">Mouse Out</button>
+//       </div>
+//     </div>
+//     <div class="keypressbtn">
+//       <h1 id="display">hello</h1>
+//       <input type="text" id="inputbox" placeholder="type something..." />
+//     </div>
+//     <div id="press_key">
+//       <div id="displaythis">Press any key</div>
+//     </div>
+
+//     <script src="eventListener.js"></script>
+//   </body>
+// </html>
+// CSS
+// body {
+//   background-color: rgb(41, 128, 146);
+// }
+// .wrapper {
+//   border: 2px solid black;
+//   background-color: rgb(16, 145, 185);
+// }
+// .btn {
+//   display: flex;
+//   justify-content: space-between;
+//   align-items: center;
+//   color: #000;
+//   padding: 0.8em 0.5em;
+//   border: 1px solid black;
+//   font-size: 0.9rem;
+//   font-weight: 500;
+// }
+// #display {
+//   color: blanchedalmond;
+//   text-align: center;
+// }
+
+// #inputbox {
+//   margin: 0 0 0.5em 7em;
+//   font-style: oblique;
+//   padding: 0.5em 1em;
+//   font-size: 1.5rem;
+// }
+// #displaythis {
+//   font-size: 2rem;
+//   text-align: center;
+// }
